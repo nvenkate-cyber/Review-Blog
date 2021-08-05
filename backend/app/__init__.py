05d4fe6dc4554eaa3322d33c7895cf90859bb1bc
vcoetoG <vcoeto@hotmail.com>
2021-08-05 17:24:38 -0500

    deleted unnecesary comment

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -7,7 +7,6 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 app.config["MONGO_URI"] = "mongodb://localhost:27017/Database"
 mongo = PyMongo(app)
 
