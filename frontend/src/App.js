5f4d787c061630687bc28cf521f0c8923e26a09d
Nandini Venkatesh <nandi@Venkatesh.Ascend.local>
2021-08-04 23:52:21 -0400

    add home page in react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,88 @@
 import logo from './logo.svg';
+import img from './img.svg';
+import React, {Component} from 'react'
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { Link } from '@material-ui/core';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Browse from './pages/browse';
+import Login from './pages/login';
+import Register from './pages/register';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 17,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function App() {
+  const classes = useStyles();
+
   return (
+    <>
+    <head>
+      <title>Review Blog</title>
+    </head>
+    
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/home' exact component={Home} />
+        <Route path='/browse' component={Browse} />
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+      </Switch>
+    </Router>
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>WELCOME!</h1>
+        <h2>TRENDING REVIEWS:</h2>
+      <Card className="card" style={{ width: '18rem'}}>
+        <CardContent style={{backgroundColor: "#FFF4F4"}}>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>Top Reviewed Movie</Typography>
+          <Typography variant="h5" component="h2">Disney's Jungle Cruise</Typography>
+          <Typography className={classes.pos} color="textSecondary">Adventure/Fantasy</Typography>
+          <Typography variant="body2" component="p">4.1/5</Typography>
+        </CardContent>
+        <CardActions style={{backgroundColor: "#FFF4F4"}}>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+      <br></br>
+      <Card className="card" style={{ width: '18rem' }}>
+        <CardContent style={{backgroundColor: "#FFF4F4"}}>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>Top Reviewed Song</Typography>
+          <Typography variant="h5" component="h2">Good 4 U</Typography>
+          <Typography className={classes.pos} color="textSecondary">Olivia Rodrigo</Typography>
+          <Typography variant="body2" component="p">Album: Sour</Typography>
+        </CardContent>
+        <CardActions style={{backgroundColor: "#FFF4F4"}}>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+        
       </header>
     </div>
+    </>
   );
 }
 
---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from './NavbarElements';
+  
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+  
+        <NavMenu>
+          <NavLink to='/home' activeStyle>
+            HOME
+          </NavLink>
+          <NavLink to='/browse' activeStyle>
+            BROWSE
+          </NavLink>
+          <NavLink to='/login' activeStyle>
+            LOGIN
+          </NavLink>
+          <NavLink to='/register' activeStyle>
+            REGISTER
+          </NavLink>
+          {/* Second Nav */}
+          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+  
+export default Navbar;
