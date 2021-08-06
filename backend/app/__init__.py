3ea6c28ce75f22c2b04db9b108734e7ac8ff7c66
José Joaquín Zubieta Rico <jose.zubieta@cimat.mx>
2021-08-06 16:34:38 -0500

    add search logic for backend

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_pymongo import PyMongo
 
-# import bcrypt
+
+from api.v1.consume_itunes import get_response
 
 app = Flask(__name__)
 
@@ -86,6 +87,16 @@ def login():
             return error, 418
 
     return render_template("login.html", title="Login", url=os.getenv("URL"))
+
+
+@app.route("/search", methods=["POST", "GET"])
+def search():
+    if request.method == "POST":
+        query = request.form["query"]
+
+        ok, response = get_response(query=query)
+
+    return render_template("search.html", title="Login", url=os.getenv("URL"))
 
 
 @app.route("/health")
